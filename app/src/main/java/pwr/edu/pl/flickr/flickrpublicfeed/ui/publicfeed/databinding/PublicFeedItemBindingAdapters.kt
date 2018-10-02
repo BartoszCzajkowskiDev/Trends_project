03bbd0f4e3dfa831660ad3e523424626c4660fb3
bartoszczajkowski <bpczajkowski@gmail.com>
2018-10-02 11:23:12 +0200

    #project_commit

---
+++ app/src/main/java/pwr/edu/pl/flickr/flickrpublicfeed/ui/publicfeed/databinding/PublicFeedItemBindingAdapters.kt
@@ -0,0 +1,26 @@
+package pwr.edu.pl.flickr.flickrpublicfeed.ui.publicfeed.databinding
+
+import android.databinding.BindingAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by Michal S. on 05.12.2017.
+ * Bindings for Public Feed items on the list
+ */
+@BindingAdapter("publishedDateBind")
+fun bindPublishedDate(textView: TextView, date: Date?) {
+    if (date != null) {
+        textView.text = SimpleDateFormat.getDateTimeInstance().format(date)
+    }
+}
+
+@BindingAdapter("publishedImgBind")
+fun bindPublishedDate(imageView: ImageView, url:String) {
+    Picasso.with(imageView.context)
+            .load(url)
+            .into(imageView)
+}
